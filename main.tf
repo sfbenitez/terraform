aab0e705763b33150e86cfb409725f36264464f9
Ferrete <sergioferretebenitez@gmail.com>
2018-02-15 01:14:53 +0100

    Added some comments.

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "openstack" {
 }
 
+## node: orbit
 resource "openstack_compute_instance_v2" "orbit" {
   name            = "orbit"
   image_id        = "f67e34fb-108d-4418-9a49-4a2dbde5a8f1"
@@ -13,15 +14,20 @@ resource "openstack_compute_instance_v2" "orbit" {
   }
 }
 
+# get float ip from pool
 # resource "openstack_networking_floatingip_v2" "getfip_1" {
 #   pool = "ext-net"
 # }
 
+# associate existing float IP
 resource "openstack_compute_floatingip_associate_v2" "fip_1" {
   floating_ip = "172.22.201.108"
+  # associate new float ip requested to the pool
+  # floating_ip = "${openstack_networking_floatingip_v2.getfip_1.address}"
   instance_id = "${openstack_compute_instance_v2.orbit.id}"
 }
 
+# new volume, not attached
 resource "openstack_blockstorage_volume_v2" "openstack" {
   name        = "openstack"
   description = "Volume create by terraform"
@@ -36,7 +42,7 @@ resource "openstack_blockstorage_volume_v2" "openstack" {
 #   ip_address = "10.0.0.16"
 # }
 
-
+# node: mint
 resource "openstack_compute_instance_v2" "mint" {
   name            = "mint"
   image_id        = "f67e34fb-108d-4418-9a49-4a2dbde5a8f1"
@@ -49,11 +55,13 @@ resource "openstack_compute_instance_v2" "mint" {
   }
 }
 
+# associate existing float IP
 resource "openstack_compute_floatingip_associate_v2" "fip_2" {
   floating_ip = "172.22.201.111"
   instance_id = "${openstack_compute_instance_v2.mint.id}"
 }
 
+# node: boomer
 resource "openstack_compute_instance_v2" "boomer" {
   name            = "boomer"
   image_id        = "f67e34fb-108d-4418-9a49-4a2dbde5a8f1"
@@ -66,10 +74,7 @@ resource "openstack_compute_instance_v2" "boomer" {
   }
 }
 
-# resource "openstack_networking_floatingip_v2" "getfip_1" {
-#   pool = "ext-net"
-# }
-
+# associate existing float IP
 resource "openstack_compute_floatingip_associate_v2" "fip_3" {
   floating_ip = "172.22.200.207"
   instance_id = "${openstack_compute_instance_v2.boomer.id}"
