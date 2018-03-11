596d144990f5a324412dc8ab81d310337271caa3
Ferrete <sergioferretebenitez@gmail.com>
2018-03-11 15:27:31 +0100

    Added 3º node to kubeadm

--- openstack-kolla-ansible/main.tf
+++ openstack-kolla-ansible/main.tf
@@ -17,7 +17,6 @@ resource "openstack_networking_subnet_v2" "subnet-openstack" {
 resource "openstack_compute_secgroup_v2" "openstack-secgroup" {
   name        = "openstack"
   description = "Sec Group for openstack deployment"
-
   rule {
     from_port   = 22
     to_port     = 22
@@ -26,14 +25,24 @@ resource "openstack_compute_secgroup_v2" "openstack-secgroup" {
   }
 }
 
-resource "openstack_networking_secgroup_rule_v2" "permit-local-trafic" {
+resource "openstack_networking_secgroup_rule_v2" "openstack-local-trafic" {
   direction         = "ingress"
   ethertype         = "IPv4"
-  # protocol          = "tcp"
-  # port_range_min    = 22
-  # port_range_max    = 22
-  # remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = "${openstack_compute_secgroup_v2.openstack-secgroup.id}"
+  remote_group_id = "${openstack_compute_secgroup_v2.openstack-secgroup.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "openstack-remote-trafic" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  security_group_id = "${openstack_compute_secgroup_v2.openstack-secgroup.id}"
+  remote_group_id = "84865709-76a7-4dd8-9807-2d10481e88ea"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "default-remote-trafic" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  security_group_id = "84865709-76a7-4dd8-9807-2d10481e88ea"
   remote_group_id = "${openstack_compute_secgroup_v2.openstack-secgroup.id}"
 }
 
@@ -72,7 +81,7 @@ resource "openstack_networking_port_v2" "orbit_ext_port" {
 resource "openstack_compute_instance_v2" "orbit" {
   name            = "orbit"
   image_id        = "0dc7dc47-d3b6-43fa-ba67-0d3242f948f3"
-  flavor_id       = "45" # 4G RAM
+  flavor_id       = "44" # 4G RAM
   key_pair        = "id_rsa"
   security_groups = ["default", "${openstack_compute_secgroup_v2.openstack-secgroup.name}"]
 
@@ -86,35 +95,14 @@ resource "openstack_compute_instance_v2" "orbit" {
   }
 }
 
-# get float ip from pool
-# resource "openstack_networking_floatingip_v2" "getfip_1" {
-#   pool = "ext-net"
-# }
-
 # associate existing float IP
 resource "openstack_compute_floatingip_associate_v2" "fip_1" {
   floating_ip = "172.22.201.108"
-  # associate new float ip requested from the pool
-  # floating_ip = "${openstack_networking_floatingip_v2.getfip_1.address}"
   instance_id = "${openstack_compute_instance_v2.orbit.id}"
-}
-
-# create new volume, not attached
-resource "openstack_blockstorage_volume_v2" "ceph_vol_1" {
-  name        = "ceph_vol_1"
-  description = "Kolla-ansible ceph_vol_1"
-  size        = 10
-}
-
-# attach volume to orbit
-resource "openstack_compute_volume_attach_v2" "attachedtorobit" {
-  instance_id = "${openstack_compute_instance_v2.orbit.id}"
-  volume_id = "${openstack_blockstorage_volume_v2.ceph_vol_1.id}"
 }
 
 
 # node: mint, COMPUTE node
-
 resource "openstack_networking_port_v2" "mint_int_port" {
   # name               = "port_1"
   network_id         = "${openstack_networking_network_v2.interna-openstack.id}"
@@ -146,10 +134,25 @@ resource "openstack_networking_port_v2" "mint_ext_port" {
   }
 }
 
+resource "openstack_networking_port_v2" "mint_ext_port2" {
+  # name               = "port_1"
+  network_id         = "0276f065-e76f-4ad9-9826-5c3f01375492"
+  admin_state_up     = "true"
+  security_group_ids = ["84865709-76a7-4dd8-9807-2d10481e88ea"]
+
+  allowed_address_pairs {
+    ip_address = "192.168.200.0/24"
+  }
+  fixed_ip {
+    "subnet_id"  = "4b96837b-dcd3-4089-aad8-26f6a126c89f"
+    "ip_address" = "10.0.0.100"
+  }
+}
+
 resource "openstack_compute_instance_v2" "mint" {
   name            = "mint"
   image_id        = "0dc7dc47-d3b6-43fa-ba67-0d3242f948f3"
-  flavor_id       = "45" # 8G RAM
+  flavor_id       = "16" # 8G RAM
   key_pair        = "id_rsa"
   security_groups = ["default", "${openstack_compute_secgroup_v2.openstack-secgroup.name}"]
 
@@ -160,6 +163,11 @@ resource "openstack_compute_instance_v2" "mint" {
   network {
     port = "${openstack_networking_port_v2.mint_int_port.id}"
   }
+  network {
+    name = "red de sergio.ferrete"
+    port = "${openstack_networking_port_v2.mint_ext_port2.id}"
+  }
+
 
 }
 
@@ -169,82 +177,14 @@ resource "openstack_compute_floatingip_associate_v2" "fip_2" {
   instance_id = "${openstack_compute_instance_v2.mint.id}"
 }
 # create new volume, not attached
-resource "openstack_blockstorage_volume_v2" "ceph_vol_3" {
-  name        = "ceph_vol_3"
-  description = "Kolla-ansible ceph_vol_3"
-  size        = 10
+resource "openstack_blockstorage_volume_v2" "cinder_vol" {
+  name        = "cinder_vol"
+  description = "Kolla-ansible conder_vol"
+  size        = 30
 }
 
 # attach volume to mint
 resource "openstack_compute_volume_attach_v2" "attachedmint" {
   instance_id = "${openstack_compute_instance_v2.mint.id}"
-  volume_id = "${openstack_blockstorage_volume_v2.ceph_vol_3.id}"
-}
-
-
-#
-# # node: boomer
-resource "openstack_networking_port_v2" "boomer_ext_port" {
-  # name               = "port_1"
-  network_id         = "0276f065-e76f-4ad9-9826-5c3f01375492"
-  admin_state_up     = "true"
-  security_group_ids = ["84865709-76a7-4dd8-9807-2d10481e88ea"]
-  allowed_address_pairs {
-    ip_address = "192.168.200.0/24"
-  }
-  fixed_ip {
-    "subnet_id"  = "4b96837b-dcd3-4089-aad8-26f6a126c89f"
-    "ip_address" = "10.0.0.32"
-  }
-}
-
-
-resource "openstack_networking_port_v2" "boomer_int_port" {
-  # name               = "port_1"
-  network_id         = "${openstack_networking_network_v2.interna-openstack.id}"
-  admin_state_up     = "true"
-  security_group_ids = ["${openstack_compute_secgroup_v2.openstack-secgroup.id}"]
-
-  allowed_address_pairs {
-    ip_address = "10.0.0.0/24"
-  }
-  fixed_ip {
-    "subnet_id"  = "${openstack_networking_subnet_v2.subnet-openstack.id}"
-    "ip_address" = "192.168.200.5"
-  }
-}
-
-resource "openstack_compute_instance_v2" "boomer" {
-  name            = "boomer"
-  image_id        = "0dc7dc47-d3b6-43fa-ba67-0d3242f948f3"
-  flavor_id       = "13" # 512 RAM
-  key_pair        = "id_rsa"
-  security_groups = ["default","${openstack_compute_secgroup_v2.openstack-secgroup.name}"]
-
-  network {
-    name = "red de sergio.ferrete"
-    port = "${openstack_networking_port_v2.boomer_ext_port.id}"
-  }
-  network {
-    port = "${openstack_networking_port_v2.boomer_int_port.id}"
-  }
-}
-
-# boomer float IP
-resource "openstack_compute_floatingip_associate_v2" "fip_3" {
-  floating_ip = "172.22.200.207"
-  instance_id = "${openstack_compute_instance_v2.boomer.id}"
-}
-
-# create new volume, not attached
-resource "openstack_blockstorage_volume_v2" "ceph_vol_2" {
-  name        = "ceph_vol_2"
-  description = "Kolla-ansible ceph_vol_2"
-  size        = 10
-}
-
-# attach volume to boomer
-resource "openstack_compute_volume_attach_v2" "attachedboomer" {
-  instance_id = "${openstack_compute_instance_v2.boomer.id}"
-  volume_id = "${openstack_blockstorage_volume_v2.ceph_vol_2.id}"
+  volume_id = "${openstack_blockstorage_volume_v2.cinder_vol.id}"
 }
--- openstack-kubeadm/main.tf
+++ openstack-kubeadm/main.tf
@@ -27,26 +27,39 @@ resource "openstack_compute_floatingip_associate_v2" "fip_1" {
   instance_id = "${openstack_compute_instance_v2.orbit.id}"
 }
 
+## node: trident
+resource "openstack_compute_instance_v2" "trident" {
+  name            = "trident"
+  image_id        = "f67e34fb-108d-4418-9a49-4a2dbde5a8f1"
+  flavor_id       = "44"
+  key_pair        = "id_rsa"
+  security_groups = ["default"]
+
+  network {
+    name = "red de sergio.ferrete"
+  }
+}
+
+# associate existing float IP
+resource "openstack_compute_floatingip_associate_v2" "fip_4" {
+  floating_ip = "172.22.200.116"
+  # associate new float ip requested to the pool
+  # floating_ip = "${openstack_networking_floatingip_v2.getfip_1.address}"
+  instance_id = "${openstack_compute_instance_v2.trident.id}"
+}
+
 # create new volume, not attached
-resource "openstack_blockstorage_volume_v2" "openstack" {
-  name        = "openstack"
+resource "openstack_blockstorage_volume_v2" "gluster_1" {
+  name        = "gluster-1"
   description = "Volume create by terraform"
   size        = 5
 }
 
 # attach volume to orbit
-resource "openstack_compute_volume_attach_v2" "attachedtorobit" {
-  instance_id = "${openstack_compute_instance_v2.orbit.id}"
-  volume_id = "${openstack_blockstorage_volume_v2.openstack.id}"
+resource "openstack_compute_volume_attach_v2" "attachedtotrident" {
+  instance_id = "${openstack_compute_instance_v2.trident.id}"
+  volume_id = "${openstack_blockstorage_volume_v2.gluster_1.id}"
 }
-
-# Necesita el initiator iSCSI
-# resource "openstack_blockstorage_volume_attach_v2" "va_1" {
-#   volume_id = "${openstack_blockstorage_volume_v2.openstack.id}"
-#   device = "auto"
-#   host_name = "orbit"
-#   ip_address = "10.0.0.16"
-# }
 
 # node: mint
 resource "openstack_compute_instance_v2" "mint" {
@@ -67,6 +80,19 @@ resource "openstack_compute_floatingip_associate_v2" "fip_2" {
   instance_id = "${openstack_compute_instance_v2.mint.id}"
 }
 
+# create new volume, not attached
+resource "openstack_blockstorage_volume_v2" "gluster_2" {
+  name        = "gluster-2"
+  description = "Volume create by terraform"
+  size        = 5
+}
+
+# attach volume to mint
+resource "openstack_compute_volume_attach_v2" "attachedtomint" {
+  instance_id = "${openstack_compute_instance_v2.mint.id}"
+  volume_id = "${openstack_blockstorage_volume_v2.gluster_2.id}"
+}
+
 # node: boomer
 resource "openstack_compute_instance_v2" "boomer" {
   name            = "boomer"
@@ -84,4 +110,17 @@ resource "openstack_compute_instance_v2" "boomer" {
 resource "openstack_compute_floatingip_associate_v2" "fip_3" {
   floating_ip = "172.22.200.207"
   instance_id = "${openstack_compute_instance_v2.boomer.id}"
+}
+
+# create new volume, not attached
+resource "openstack_blockstorage_volume_v2" "gluster_3" {
+  name        = "gluster-3"
+  description = "Volume create by terraform"
+  size        = 5
+}
+
+# attach volume to boomer
+resource "openstack_compute_volume_attach_v2" "attachedtoboomer" {
+  instance_id = "${openstack_compute_instance_v2.boomer.id}"
+  volume_id = "${openstack_blockstorage_volume_v2.gluster_3.id}"
 }
